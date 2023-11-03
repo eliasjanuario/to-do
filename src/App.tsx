b75dae1aba010d5d2c74efa4b067e849603899d8
elias.januario <elias.januario@farfetch.com>
2023-11-03 20:00:17 +0000

    feat: create task box and methods to delete and update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./global.css"
 
-import { useState } from "react"
+import uuid from "react-uuid"
+import { ChangeEvent, useState } from "react"
 
 import { Header } from "./components/Header"
 import { Form } from "./components/Form"
 import { List } from "./components/List"
 
 export interface Tasks {
+	id: string;
 	checked: boolean;
 	description: string;
 }
@@ -15,14 +17,37 @@ function App() {
 	const [tasks, setTasks] = useState<Tasks[]>([])
 
 	function handleNewTask(task: string) {
-		setTasks([...tasks, { checked: false, description: task}])
+		setTasks([...tasks, { id: uuid(), checked: false, description: task}])
+	}
+
+	function handleTaskChange(event: ChangeEvent<HTMLInputElement>, taskId: string) {
+		const updatedTasks = [...tasks];
+
+		const taskToUpdate = updatedTasks.find((taskItem) => taskItem.id === taskId);
+
+		if (taskToUpdate) {
+			taskToUpdate.checked = event.target.checked;
+			setTasks(updatedTasks);
+		}
+		
+	}
+
+	function handleDeletetask(taskId: string) {
+		const updatedTasks = [...tasks];
+		const indexToRemove = updatedTasks.findIndex((taskItem) => taskItem.id === taskId);
+		updatedTasks.splice(indexToRemove, 1);
+		setTasks(updatedTasks);
 	}
 
 	return (
 		<div>
 			<Header />
 			<Form handleNewTask={handleNewTask} />
-			<List tasks={tasks} />
+			<List 
+				tasks={tasks} 
+				handleDeletetask={handleDeletetask} 
+				handleTaskChange={handleTaskChange}
+			/>
 		</div>
 	)
 }
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,12 +1,17 @@
+import { ChangeEvent } from "react";
+import { Trash } from "phosphor-react";
+
 import { Tasks } from "../App"
 
 import styles from "./List.module.css"
 
 interface ListProps {
     tasks: Tasks[];
+    handleDeletetask: (taskId: string) => void;
+    handleTaskChange: (event: ChangeEvent<HTMLInputElement>, taskId: string) => void;
 }
 
-export function List({ tasks }: ListProps) {
+export function List({ tasks, handleDeletetask, handleTaskChange }: ListProps) {
 
     return (
         <div className={styles.listContainer}>
@@ -23,16 +28,21 @@ export function List({ tasks }: ListProps) {
 
             <section>
                 {tasks.length === 0 && (
-                    <div>
+                    <div className={styles.taskDisclaimer}>
                         <h4>Você ainda não tem tarefas cadastradas</h4>
                         <p>Crie tarefas e organize seus itens a fazer</p>
                     </div> 
                 )}
 
                 {tasks.length > 0 && tasks.map((task, index) => (
-                    <div key={index}>
-                        <input type="checkbox" name="" id="" />
-                        <p>{task.description}</p>
+                    <div key={index} className={styles.checkboxContainer}>
+                        <input type="checkbox" name={`checkbox_${task.id}`} id={`checkbox_${task.id}`} onChange={(e) => handleTaskChange(e, task.id)} />
+                        {task.checked ? (
+                            <p className={styles.taskDone}>{task.description}</p>
+                        ) : (
+                            <p>{task.description}</p>
+                        )}
+                        <Trash onClick={() => handleDeletetask(task.id)} />
                     </div>
                 ))}
             </section>
